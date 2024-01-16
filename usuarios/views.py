958bdb5995a2ee83276691221574c2212d62be2a
Irene <irenesilvafranca2016@gmail.com>
2024-01-15 23:40:25 -0300

    projeto python

---
+++ usuarios/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.contrib import auth
+
+# Create your views here.
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'Senha e confirmar senha não coincidem')
+            return redirect('/usuarios/cadastro')
+
+        user =User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Usuário já existe')
+            return redirect('/usuarios/cadastro')
+
+        try:
+            User.objects.create_user(
+                username=username,
+                password=senha
+            )
+            return redirect('/usuarios/login')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro onterno do servidor')
+            return redirect('/usuarios/cadastro')
+
+        def login(request):
+            if request.method == "GET":
+                return render(request, 'login.html')
+
+
+def logar(request):
+    if request.method =="GET":
+        print(request.user)
+        return render(request, 'login.html')
+    elif request.method == "POST":
+
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            messages.add_message(request, constants.SUCCESS, 'Logado!' )
+            return redirect('/flashcard/novo_flashcard')# vai dar erro ainda
+        else:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválida')
+            return redirect('/usuarios/logar')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/usuarios/logar')
+    # Implementação da visualização de login
+    #return HttpResponse('Teste')
